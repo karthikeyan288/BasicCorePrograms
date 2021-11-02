31a2f8e5689b567c2fb72ed9a1504af89b98db12
“karthikeyan288” <karthikeyanak28@gmail.com>
2021-11-02 07:44:24 +0530

    ADDING :to find the power of two

---
+++ src/com/BasicPrograms/ext/PowerOfTwo.java
@@ -0,0 +1,28 @@
+package com.BasicPrograms.ext;
+
+import java.util.Scanner;
+
+public class PowerOfTwo {
+	public static void main(String[] args) {
+		Scanner number= new Scanner(System.in);
+		System.out.println(" Enter the  numbers  x  :");
+		int value=number.nextInt();
+			for (int i = 0; i*2 <= value; i++) {
+				int x=2*i;
+				System.out.println("2 * " +i+ " = "+x);
+				}
+			
+	}
+}			
+			
+		//if (value<=31) {	
+		//}else {
+			//System.out.println("The number is above 31");
+		//}
+		
+		
+		
+		
+	
+
+
